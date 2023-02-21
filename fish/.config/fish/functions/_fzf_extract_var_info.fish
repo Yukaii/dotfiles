1d65c0e01caf37a0c52d0214267e902b2c82e948
Yukai Huang <yukaihuangtw@gmail.com>
2023-02-21 18:38:59 +0800

    fisher: udpates

--- fish/.config/fish/functions/_fzf_extract_var_info.fish
+++ fish/.config/fish/functions/_fzf_extract_var_info.fish
@@ -1,5 +1,5 @@
-# helper function for __fzf_search_shell_variables
-function __fzf_extract_var_info --argument-names variable_name set_show_output --description "Extract and reformat lines pertaining to \$variable_name from \$set_show_output."
+# helper function for _fzf_search_variables
+function _fzf_extract_var_info --argument-names variable_name set_show_output --description "Extract and reformat lines pertaining to \$variable_name from \$set_show_output."
     # Extract only the lines about the variable, all of which begin with either
     # $variable_name: ...or... $variable_name[
     string match --regex "^\\\$$variable_name(?::|\[).*" <$set_show_output |
@@ -11,5 +11,5 @@ function __fzf_extract_var_info --argument-names variable_name set_show_output -
         #   [1]: |value|
         # ...with...
         #   [1] value
-        string replace --regex ": \|(.*)\|" ' \$1'
+        string replace --regex ": \|(.*)\|" ' $1'
 end
--- fish/.config/fish/functions/_fzf_search_git_status.fish
+++ fish/.config/fish/functions/_fzf_search_git_status.fish
@@ -1,13 +1,14 @@
-function __fzf_search_git_status --description "Search the output of git status. Replace the current token with the selected file paths."
+function _fzf_search_git_status --description "Search the output of git status. Replace the current token with the selected file paths."
     if not git rev-parse --git-dir >/dev/null 2>&1
-        echo '__fzf_search_git_status: Not in a git repository.' >&2
+        echo '_fzf_search_git_status: Not in a git repository.' >&2
     else
         set selected_paths (
             # Pass configuration color.status=always to force status to use colors even though output is sent to a pipe
             git -c color.status=always status --short |
-            fzf --ansi \
+            _fzf_wrapper --ansi \
                 --multi \
                 --query=(commandline --current-token) \
+                --preview='_fzf_preview_changed_file {}' \
                 $fzf_git_status_opts
         )
         if test $status -eq 0
@@ -25,7 +26,7 @@ function __fzf_search_git_status --description "Search the output of git status.
                 end
             end
 
-            commandline --current-token --replace -- (string escape -- $cleaned_paths | string join ' ')
+            commandline --current-token --replace -- (string join ' ' $cleaned_paths)
         end
     end
 
