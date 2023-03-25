dce4bfa37735b65ba16c8df2f26001a5840e0fb7
Yukai Huang <yukaihuangtw@gmail.com>
2023-03-25 09:37:37 +0800

    feat: extend toggle term command

--- astronvim/.config/nvim/lua/user/mappings.lua
+++ astronvim/.config/nvim/lua/user/mappings.lua
@@ -3,6 +3,8 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+
+
 return {
   -- first key is the mode
   n = {
@@ -21,6 +23,20 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>gy"] = { desc = "Copy GitHub URL" },
+    ["<leader>tg"] = {
+      function()
+        require("astronvim.utils").toggle_term_cmd("tig")
+      end,
+      desc = "Open tig",
+    },
+    ["<leader>tt"] = {
+      -- "<cmd>TermExec cmd=btop<cr>",
+      function()
+        require("astronvim.utils").toggle_term_cmd("btop")
+      end,
+      desc = "Open btop"
+    }
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
