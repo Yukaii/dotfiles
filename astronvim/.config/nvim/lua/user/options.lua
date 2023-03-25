1625a1c386811c269ca3cae41f0dca657db53a78
Yukai Huang <yukaihuangtw@gmail.com>
2023-03-25 10:25:51 +0800

    feat: cursorcolumn

--- astronvim/.config/nvim/lua/user/options.lua
+++ astronvim/.config/nvim/lua/user/options.lua
@@ -8,6 +8,7 @@ return {
     signcolumn = "auto",   -- sets vim.opt.signcolumn to auto
     wrap = true,           -- sets vim.opt.wrap
     guifont = "JetbrainsMono Nerd Font:h17:#h-normal",
+    cursorcolumn = true,
   },
   g = {
     mapleader = " ",                 -- sets vim.g.mapleader
