207592ce87cd039fa6549c6474744de0408e1681
Yukai Huang <yukaihuangtw@gmail.com>
2023-04-03 11:42:21 +0800

    neovim: bufferline

--- astronvim/.config/nvim/lua/user/plugins/community.lua
+++ astronvim/.config/nvim/lua/user/plugins/community.lua
@@ -6,9 +6,12 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.colorscheme.kanagawa",          enabled = true },
-  { import = "astrocommunity.editing-support.zen-mode-nvim", enabled = true },
-  { import = "astrocommunity.motion.nvim-surround",          enabled = true },
-  { import = "astrocommunity.project.nvim-spectre",          enabled = true },
-  { import = "astrocommunity.markdown-and-latex.glow-nvim",  enabled = true }
+  { import = "astrocommunity.colorscheme.kanagawa",               enabled = true },
+  { import = "astrocommunity.editing-support.zen-mode-nvim",      enabled = true },
+  { import = "astrocommunity.motion.nvim-surround",               enabled = true },
+  { import = "astrocommunity.project.nvim-spectre",               enabled = true },
+  { import = "astrocommunity.markdown-and-latex.glow-nvim",       enabled = true },
+  { import = "astrocommunity.editing-support.todo-comments-nvim", enabled = true },
+  { import = "astrocommunity.editing-support.nvim-ts-rainbow2",   enabled = true },
+  { import = "astrocommunity.bars-and-lines.bufferline-nvim",     enabled = true },
 }
