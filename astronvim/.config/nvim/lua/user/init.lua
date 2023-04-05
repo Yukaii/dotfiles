42d015ec046ba652131517c34893cf5ffe9f7718
Yukai Huang <yukaihuangtw@gmail.com>
2023-04-05 12:50:38 +0800

    neovim: try monokai-pro

--- astronvim/.config/nvim/lua/user/init.lua
+++ astronvim/.config/nvim/lua/user/init.lua
@@ -17,7 +17,7 @@ return {
     },
   },
   -- Set colorscheme to use
-  colorscheme = "kanagawa",
+  colorscheme = "monokai-pro",
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
     virtual_text = false,
