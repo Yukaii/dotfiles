08d85afb2b2df4a9692df45ecc59c2226606f8fb
Yukai Huang <yukaihuangtw@gmail.com>
2023-04-15 20:04:48 +0800

    neovim: add neogit commnuity plugin

--- astronvim/.config/nvim/lua/user/plugins/community.lua
+++ astronvim/.config/nvim/lua/user/plugins/community.lua
@@ -14,5 +14,41 @@ return {
   { import = "astrocommunity.editing-support.todo-comments-nvim",    enabled = true },
   { import = "astrocommunity.editing-support.nvim-ts-rainbow2",      enabled = true },
   { import = "astrocommunity.bars-and-lines.heirline-vscode-winbar", enabled = true },
+  { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = 120,
+      disabled_filetypes = { "help" },
+    },
+  },
+  { import = "astrocommunity.git.neogit" },
+  { import = "astrocommunity.media.pets-nvim" },
+  {
+    "giusgad/pets.nvim",
+    opts = {
+      row = 8,
+      default_pet = "slime",
+      default_style = "green",
+      random = false,
+      popup = {
+        winblend = 100,
+        avoid_statusline = true
+      }
+    }
+  },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp",            enabled = true },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   config = function()
+  --     require("copilot").setup({
+  --       filetypes = {
+  --         markdown = true,
+  --         yaml = true,
+  --       },
+  --       copilot_node_command: ~/.volta/tools/image/node/18.14.2/bin/node
+  --     })
+  --   end
+  -- }
   -- { import = "astrocommunity.motion.leap-nvim",                      enabled = true },
 }
