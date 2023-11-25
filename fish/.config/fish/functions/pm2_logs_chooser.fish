8894bf62a002564375741e45de77253a0d45d2dc
Yukai Huang <yukaihuangtw@gmail.com>
2023-11-25 18:51:00 +0800

    pm2lc: support different config file

--- fish/.config/fish/functions/pm2_logs_chooser.fish
+++ fish/.config/fish/functions/pm2_logs_chooser.fish
@@ -1,12 +1,34 @@
 function pm2_logs_chooser
-    # Check if ecosystem.config.js exists
-    if test -f ecosystem.config.js
-        # Use Node.js to extract application names and pipe to fzf
-        set APP_NAME (node -e "console.log(JSON.stringify(require('./ecosystem.config.js').apps.map(app => app.name)))" | jq -r '.[]' | fzf)
+    argparse 'appName=' 'config=' -- $argv
+    or return
 
-        # Run pm2 logs for the selected application
+    # Default to known config file names if not set
+    if not set -q _flag_config
+        if test -f ecosystem.config.js
+            set _flag_config ecosystem.config.js
+        else if test -f ecosystem.config.cjs
+            set _flag_config ecosystem.config.cjs
+        end
+    end
+
+    if not set -q _flag_config
+        echo "No ecosystem configuration file found."
+        return
+    end
+
+    # If an app name is provided, use it directly
+    if set -q _flag_appName
+        set APP_NAME $_flag_appName
+    else
+        # Use Node.js to extract application names and pipe to fzf
+        set APP_NAME (node -e "console.log(JSON.stringify(require('./$_flag_config').apps.map(app => app.name)))" | jq -r '.[]' | fzf)
+    end
+
+    # Check if an app was selected or provided
+    if test -n "$APP_NAME"
+        # Run pm2 logs for the selected or provided application
         pm2 logs $APP_NAME
     else
-        echo "ecosystem.config.js not found."
+        echo "No application selected."
     end
 end
