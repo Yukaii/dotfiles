06e832c03b9eae29d1edb3700ea8856b2df2be8f
Yukai Huang <yukaihuangtw@gmail.com>
2023-12-15 16:46:04 +0800

    wezterm: refactor color_scheme module

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm';
+local color_scheme_module = require 'color_scheme';
 
 local default_color_scheme = "Kanagawa (Gogh)"
 
@@ -9,36 +10,7 @@ local color_scheme = "Kanagawa (Gogh)"
 -- local color_scheme = "kanagawabones"
 
 -- init colors variable
-local colors = {}
-
--- check if color scheme exists
-local function isColorSchemeAvailableAsBultin(c)
-  local color_schemes = wezterm.get_builtin_color_schemes()
-  for key in pairs(color_schemes) do
-    if key == c then
-      return true
-    end
-  end
-  return false
-end
-
--- get colors with fallback to default color scheme
-if isColorSchemeAvailableAsBultin(color_scheme) then
-  colors = wezterm.get_builtin_color_schemes()[color_scheme]
-else
-  -- try loaded from $HOME/wezterm/config/colors/:name.toml first
-  local filepath = os.getenv("HOME") .. "/.config/wezterm/colors/" .. color_scheme .. ".toml"
-  local f = io.open(filepath, "r")
-
-  if f ~= nil then
-    io.close(f)
-    colors = wezterm.color.load_scheme(filepath)
-  else
-    -- fallback to default color scheme
-    colors = wezterm.get_builtin_color_schemes()[default_color_scheme]
-  end
-end
-
+local colors = color_scheme_module.getColorScheme(color_scheme, default_color_scheme)
 
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_left_half_circle_thick
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_right_half_circle_thick
