9fda471063a3c3b10fd26bd7bc7d24398fda7710
Yukai Huang <yukaihuangtw@gmail.com>
2024-02-11 15:51:18 +0800

    helix: make quicklist

--- bin/.bin/hx-fzf.sh
+++ bin/.bin/hx-fzf.sh
@@ -1,11 +1,25 @@
 #!/bin/sh
 
-selected_file=$(rg --line-number --column --no-heading --smart-case . | fzf --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window '~3,+{2}+3/2' | awk '{ print $1 }' | cut -d: -f1,2,3)
+selected_files=$(
+    rg --line-number --column --no-heading --smart-case . \
+    | fzf --multi --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window '~3,+{2}+3/2' --bind 'ctrl-q:select-all+accept'
+)
 
-echo $selected_file
+line_count=$(echo "$selected_files" | grep -c '^')
 
-if [ -n "$selected_file" ]; then
-    hx-wez $selected_file top
+if [ -n "$selected_files" ]; then
+    if [ "$line_count" -eq "1" ]; then
+        file=$(echo $selected_files | awk -F: '{print $1 ":" $2 ":" $3}')
+        hx-wez $file top
+    else
+        qf_tmp=$(mktemp)
+        echo "$selected_files" | awk -F: '{
+            printf "%s:%s:%s    ", $1, $2, $3
+            for (i = 4; i <= NF; i++) {
+                printf "%s", $i
+            }
+            printf "\n"
+        }' > "$qf_tmp"
+        hx-wez "$qf_tmp" top h
+    fi
 fi
-
-exit 0
