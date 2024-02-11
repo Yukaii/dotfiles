95bf95089180b811113971bb10b0d5f4cd3e5573
Yukai Huang <yukaihuangtw@gmail.com>
2024-02-11 10:17:11 +0800

    helix: cleanup

--- bin/.bin/hx-fzf.sh
+++ bin/.bin/hx-fzf.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-# selected_file=$1
-
-
 selected_file=$(rg --line-number --column --no-heading --smart-case . | fzf --delimiter : --preview 'bat --style=full --color=always --highlight-line {2} {1}' --preview-window '~3,+{2}+3/2' | awk '{ print $1 }' | cut -d: -f1,2,3)
 
 echo $selected_file
 
 if [ -n "$selected_file" ]; then
-    echo "noooo"
     hx-wez $selected_file top
 fi
 
--- bin/.bin/hx-wezterm.sh
+++ bin/.bin/hx-wezterm.sh
@@ -8,21 +8,6 @@ line_number=$(echo $status_line | awk '{ print $2}')
 
 session_name=$(echo $PWD | sed "s:$HOME:~:" | tr '/' '_')
 
-split_pane_down() {
-  bottom_pane_id=$(wezterm cli get-pane-direction down)
-  if [ -z "${bottom_pane_id}" ]; then
-    bottom_pane_id=$(wezterm cli split-pane)
-  fi
-
-  wezterm cli activate-pane-direction --pane-id $bottom_pane_id down
-
-  send_to_bottom_pane="wezterm cli send-text --pane-id $bottom_pane_id --no-paste"
-  program=$(wezterm cli list | awk -v pane_id="$bottom_pane_id" '$3==pane_id { print $6 }')
-  if [ "$program" = "lazygit" ]; then
-    echo "q" | $send_to_bottom_pane
-  fi
-}
-
 get_pane_direction() {
   general_direction=$1
 
