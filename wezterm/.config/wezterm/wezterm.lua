c21b39cb904250bf26ccfe1803df1208f7b86185
Yukai Huang <yukaihuangtw@gmail.com>
2024-05-13 00:14:49 +0800

    wezterm: transparent bg and blur

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -75,6 +75,9 @@ local config = {
 
   initial_cols = 120,
   initial_rows = 30,
+
+  window_background_opacity = 0.8,
+  macos_window_background_blur = 60,
 }
 
 font_config_module.loadFontConfig(config, font_name)
