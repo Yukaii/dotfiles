3db7753f182d625b403720f504e943c446ce6ed3
Yukai Huang <yukaihuangtw@gmail.com>
2025-10-17 14:13:27 +0800

    feat(opencode): also add confetti hooks for opencode

---
+++ opencode/.config/opencode/plugin/confetti.js
@@ -0,0 +1,11 @@
+export const ConfettiPlugin = async ({ project, client, $, directory, worktree }) => {
+
+  return {
+    event: async ({ event }) => {
+      // Send notification on session completion
+      if (event.type === "session.idle") {
+        await $`open raycast://extensions/raycast/raycast/confetti`
+      }
+    },
+  }
+}
